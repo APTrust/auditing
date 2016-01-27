3176d2f03b72a66c5869016a5d6f5770c538d708
A. Diamond <diamond@sheprador.com>
2016-01-27 16:28:29 -0500

    Added unrecorded file report

--- audit_001.py
+++ audit_001.py
@@ -8,8 +8,8 @@ import sqlite3
 import sys
 
 
-def object_report(conn, bag_name):
-    print("Checking ingest of bag {0}".format(bag_name))
+def full_object_report(conn, bag_name):
+    print("Full report for bag {0}".format(bag_name))
     query = """select
     f.ingest_record_id,
     f.unpacked_file_path,
@@ -30,7 +30,45 @@ def object_report(conn, bag_name):
     try:
         c.execute(query, values)
         rows = c.fetchall()
-        print("{0} rows", len(rows))
+        print("{0} rows".format(len(rows)))
+        for row in rows:
+            for key in row.keys():
+                print("{0:20}:  {1}".format(key, row[key]))
+            print('-' * 76)
+    except (sqlite3.Error, RuntimeError) as err:
+        print(err)
+    finally:
+        c.close()
+
+def unrecorded_file_report(conn, bag_name):
+    print("Unrecorded file report for bag {0}".format(bag_name))
+    query = """select
+    f.ingest_record_id,
+    f.unpacked_file_path,
+    f.gf_file_path,
+    f.gf_needs_save,
+    f.gf_identifier,
+    f.gf_storage_url,
+    f.fedora_file_uri,
+    f.gf_uuid,
+    f.s3_key,
+    f.glacier_key,
+    o.error_message
+    from audit_001_objects o
+    inner join audit_001_files f on f.ingest_record_id = o.ingest_record_id
+    where o.key = ?
+    and f.gf_needs_save = 1
+    and (f.s3_key is null or f.glacier_key is null or
+         f.s3_key != f.gf_uuid or f.glacier_key != f.gf_uuid or
+         substr(f.gf_storage_url, 55) != f.gf_uuid or
+         substr(f.fedora_file_uri, 55) != f.gf_uuid or
+         f.gf_storage_url != f.fedora_file_uri)"""
+    values = (bag_name,)
+    c = conn.cursor()
+    try:
+        c.execute(query, values)
+        rows = c.fetchall()
+        print("{0} rows".format(len(rows)))
         for row in rows:
             for key in row.keys():
                 print("{0:20}:  {1}".format(key, row[key]))
@@ -48,5 +86,9 @@ if __name__ == "__main__":
         sys.exit()
     conn = sqlite3.connect('db/aptrust.db')
     conn.row_factory = sqlite3.Row
-    object_report(conn, sys.argv[1])
+    full_object_report(conn, sys.argv[1])
+    print('')
+    print('*' * 76)
+    print('')
+    unrecorded_file_report(conn, sys.argv[1])
     conn.close()
